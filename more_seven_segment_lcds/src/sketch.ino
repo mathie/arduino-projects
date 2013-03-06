94a84a5c921eec763943acd297dbc949c462eff5
Graeme Mathieson <mathie@woss.name>
2013-03-06 21:36:13 +0000

    Demonstrate two separate objects each managing different digits.

--- more_seven_segment_lcds/src/sketch.ino
+++ more_seven_segment_lcds/src/sketch.ino
@@ -1,22 +1,26 @@
 #include "lcd_array.h"
 
+const unsigned hourDigitPins[] = { 1, 2, NULL };
+const unsigned minuteDigitPins[] = { 3, 4, NULL };
 
-
-const unsigned int digitPins[] = { 1, 2, 3, 4, NULL };
 const unsigned int segmentPins[] = { 5, 6, 7, 8, 9, 10, 11 };
 const unsigned int dpSegmentPin = 12;
 
-LcdArray lcdArray(digitPins, segmentPins, dpSegmentPin);
+LcdArray hourLcdArray(hourDigitPins, segmentPins, dpSegmentPin);
+LcdArray minuteLcdArray(minuteDigitPins, segmentPins, dpSegmentPin);
 
 void setup()
 {
   randomSeed(analogRead(A0));
-  const unsigned int randomNumber = random(1, 2000);
+  const unsigned int randomHour = random(0, 24);
+  const unsigned int randomMinute = random(0, 60);
 
-  lcdArray.changeNumber(randomNumber, 0);
+  hourLcdArray.changeNumber(randomHour);
+  minuteLcdArray.changeNumber(randomMinute);
 }
 
 void loop()
 {
-  lcdArray.refreshNumber();
+  hourLcdArray.refreshNumber();
+  minuteLcdArray.refreshNumber();
 }
