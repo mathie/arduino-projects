7d46f34140b5f5404c72d9b0f07880f120e6001a
Graeme Mathieson <mathie@woss.name>
2013-03-23 11:49:59 +0000

    A reset button.

--- more_seven_segment_lcds/src/sketch.ino
+++ more_seven_segment_lcds/src/sketch.ino
@@ -1,16 +1,25 @@
 #include "time_display.h"
 #include "lcd_array.h"
+#include "button.h"
 
 const unsigned int digitPins[]   = { 1, 2, 3, 4, NULL };
 const unsigned int segmentPins[] = { 5, 6, 7, 8, 9, 10, 11 };
 const unsigned int dpSegmentPin = 12;
 
+const unsigned int resetButtonPin = 13;
+
 LcdArray lcdArray(digitPins, segmentPins, dpSegmentPin);
 TimeDisplay timeDisplay(&lcdArray);
+Button resetButton(resetButtonPin);
 
-void setup() { }
+void setup() {
+}
 
 void loop()
 {
+  if(resetButton.isUpdated() && resetButton.isPressed()) {
+    timeDisplay.reset();
+  }
+
   timeDisplay.refresh();
 }
